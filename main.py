1ef145370d00e1be1c61eef2fedccc41eb21ffb5
0marAlmo <0marAlmo@users.noreply.github.com>
2017-12-24 22:35:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ def password_generator(password_length):
     (None) -> str
     '''
     
-    password= ''
-    
     gen = random.choice(string.hexdigits)
-    
-    for i in range(password_length):
-        gen = str(random.choice(string.hexdigits)) 
-        password += gen
-    
-    return password
+    repeat= input("Would you like to generate a password? (yes or no) ")
+
+    while repeat.strip().lower() == 'yes':
+        password= ''
+        for i in range(password_length):
+            gen = str(random.choice(string.hexdigits)) 
+            password += gen
+        print(password)
+
+        repeat= input("Would you like to generate a password? ")
+
 
 password_length = int(input("Enter the length of the password you would like: "))
-
+    
 print("Key generated:", password_generator(password_length))
