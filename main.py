4761be91537907a4055668d2f2910a938eb49bd8
Rawad Aboudlal <Rawadaboudlal@yahoo.com>
2017-12-24 10:27:10 -0500

    Cleaned up code to separate user interface from core functionality.
    Also made generate_password() return a string instead of a list.

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import random
-import string 
+import string
 
-
-key_length= int(input("Enter the length of the password you would like: "))
+def password_generator(password_length):
+    '''
+    (None) -> str
+    '''
     
-def password_generator():
     password= ''
-    gen= (random.choices(string.hexdigits))
-    for i in range(key_length):
-        gen= str(random.choices(string.hexdigits)) 
-        password+=gen
-        FinPass=str(''.join(password))
-    return "Key generated: " + FinPass
+    
+    gen = random.choices(string.hexdigits)
+    
+    for i in range(password_length):
+        gen = str(random.choices(string.hexdigits)) 
+        password += gen
+    
+    return password
 
-print(password_generator())
+password_length = int(input("Enter the length of the password you would like: "))
+
+print("Key generated:", password_generator(password_length))
